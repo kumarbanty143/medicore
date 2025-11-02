067612df63a9b1b50618c45285f05b6120d835b9
Himanshu kumar <himanshukumar@Himanshus-MacBook-Pro.local>
2025-11-02 18:31:37 +0530

    lint fix

--- src/components/appointments/BookingConfirmationStep.tsx
+++ src/components/appointments/BookingConfirmationStep.tsx
@@ -16,7 +16,7 @@ interface BookingConfirmationStepProps {
 }
 
 function BookingConfirmationStep({
-  selectedDentistId,
+  selectedDoctorId,
   selectedDate,
   selectedTime,
   selectedType,
@@ -45,7 +45,7 @@ function BookingConfirmationStep({
 
         <CardContent className="space-y-4">
           {/* doctor info */}
-          <DoctorInfo doctorId={selectedDentistId} />
+          <DoctorInfo doctorId={selectedDoctorId} />
 
           {/* appointment details */}
           <div className="grid grid-cols-2 gap-4 pt-4 border-t">
